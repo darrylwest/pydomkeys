3582f443a494f16aa05190b423e2d0d414b3f579
darryl west <darryl.west@raincitysoftware.com>
2023-09-14 06:20:19 -0700

    added timing and uniqueness testing to stress tests

--- tests/stress.py
+++ tests/stress.py
@@ -9,26 +9,48 @@ import time
 
 shard_count = 4
 keygen = KeyGen.create("T1", shard_count)
-max_count = 250_000
 
 # TODO(dpw): add thread pool workers with mpire
 
+def timer_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time_ns()
+        result = func(*args, **kwargs)
+        end_time = time.time_ns()
+        elapsed = (end_time - start_time) / 1_000_000_000
+        print(f"{func.__name__} took {elapsed} seconds to run.")
+        return result
+    return wrapper
 
+max_count = 250_000
+
+@timer_decorator
 def test_txkey():
+    kset = set()
+
     keys = (keygen.txkey() for _ in range(max_count))
 
+    count = 0
     for key in keys:
-        assert len(key) == 12, f"key: {key} has incorrect length {len(key)}"
+        count += 1
+        kset.add(key)
+        assert len(kset) == count, f"key: {key} was not unique, count: {count}"
+        assert len(key) == 12, f"txkey: {key} has incorrect length {len(key)}"
 
-        # TODO(dpw): test for uniqueness
-
-    return True
+    return max_count == len(kset)
 
 
+@timer_decorator
 def test_route_key():
+    kset = set()
     keys = (keygen.route_key() for _ in range(max_count))
 
+    count = 0
     for key in keys:
+        count += 1
+        kset.add(key)
+        assert len(kset) == count, f"route_key: {key} was not unique, count: {count}"
+
         assert (
             len(key) == 16
         ), f"ERROR! route key: {key} has incorrect length {len(key)}"
@@ -37,19 +59,17 @@ def test_route_key():
             shard < shard_count
         ), f"[red] ERROR! route key has a bad shard parse {shard}"
 
-        # TODO(dpw): test for uniqueness
+    return max_count == len(kset)
+    
 
 
 def main(args: list) -> None:
     # print(f'{args}')
 
-    t0 = time.time_ns()
     test_txkey()
     test_route_key()
-    t1 = time.time_ns()
-    elapsed = (t1 - t0) / 1_000_000_000
 
-    print(f"[green3]stress tests completed {max_count} rounds without error, time: {elapsed} seconds...")
+    # print(f"[green3]stress tests completed {max_count} rounds without error, time: {elapsed} seconds...")
 
 
 if __name__ == "__main__":
